7e0639b0d267101602564b26c0234d78f7d227ab
Yasaman <ys.jafari98@gmail.com>
2018-10-26 06:26:58 +0330

    Now clients can play with other.

--- client.c
+++ client.c
@@ -8,18 +8,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h> 
 
 #define BUFFER_SIZE 1024
 #define TIMEOUT 4
 #define IP_ADDRESS "239.255.255.250"
 #define INITIAL_PORT 1234
 #define NAME_MAX_LEN 64
+#define MAP_ROW 10
+#define MAP_COLUMN 10
+#define WIN_MESSAGE "You won! Congragulations!"
+#define LOSE_MESSAGE "You lost! Better luck next time!"
+#define REPEATED_MOVE "You have chosen this move before! Try another one!"
+#define FULL_BLOCK "Awesome! You have chosen a block with ship!"
+#define EMPTY_BLOCK "Oh no! You have chosen an empty block!"
 
 char buffer[BUFFER_SIZE] = {0}; 
 int clientID, clientToServerfd, clientToClientfd, gamefd, newGamefd; 
 struct sockaddr_in heartBeatAddress, dataAddress, clientAddress, gameAddress;
 char *ipInfo, *IPandPort;
 char name[NAME_MAX_LEN];
+char fileName[NAME_MAX_LEN];
+char map[(MAP_ROW * 2) * (MAP_COLUMN + 1) + 10];
 
 char* concat(const char *s1, const char *s2)
 {
@@ -75,7 +85,136 @@ void createAPortForClientsGame()
     ipInfo = concat(ipInfo, " ");
     IPandPort = concat(ipInfo, finalAdr);
     IPandPort = concat(IPandPort, " ");
-    puts(IPandPort);
+    // puts(IPandPort);
+}
+
+int checkIfFull(char *place)
+{
+    int y, x;
+    x = atoi(strtok(place, " "));
+    y = atoi(strtok(NULL, " "));
+
+    // ship exists
+    if(map[20 * y + x] == '1')
+    {
+        map[20 * y + x] = '2';
+        return 1;
+    }
+
+    //already chose
+    else if(map[20 * y + x] == '2')
+        return 2;
+     
+    //empty   
+    else
+    {
+        map[20 * y + x] = '2';
+        return 0;
+    }
+        
+}
+
+void playGame(int playerTurn, int playerNum)
+{
+    if(playerTurn == 1)
+    {
+        int fd = newGamefd;
+        //the one who asked for connection
+        if(playerNum == 2)
+            fd = gamefd;
+        char move[NAME_MAX_LEN];
+        bzero((char *) &buffer, sizeof(buffer));
+        write(1, "Please enter a location.[ 0 <= x, y <= 9 ]\n", 43);
+        int bytesCount = read(0, move, BUFFER_SIZE);
+        move[bytesCount - 1] = '\0';
+        if(send(fd, move, strlen(move), 0) < 0)
+        {
+            write(1, "Sending info to other player failed.\n", 37);
+            exit(1);
+        }
+        bzero((char *) &buffer, sizeof(buffer));
+        int bytes = recv(fd, buffer, BUFFER_SIZE, 0);
+        if(bytes < 0)
+        {
+            write(1, "Receiving info from other player failed.\n", 41);
+            exit(1);
+        } 
+        write(1, buffer, strlen(buffer));
+        write(1, "\n", 1);
+        if(strcmp(buffer, FULL_BLOCK) == 0 || strcmp(buffer, REPEATED_MOVE) == 0 )
+            playGame(1, playerNum);
+        else if(strcmp(buffer, EMPTY_BLOCK) == 0)
+            playGame(0, playerNum);
+    } 
+
+    else if(playerTurn == 0)
+    {
+        write(1, "It's your opponent's turn. Please wait...\n", 42);
+        int fd = newGamefd;
+        if(playerNum == 2)
+            fd = gamefd;
+        bzero((char *) &buffer, sizeof(buffer));
+        int bytes = recv(fd, buffer, BUFFER_SIZE, 0);
+        if(bytes < 0)
+        {
+            write(1, "Receiving info from other player failed.\n", 41);
+            exit(1);
+        } 
+        write(1, "Your opponent's choice: ", 24);
+        write(1, buffer, strlen(buffer));
+        char* moveInfo = malloc(strlen(buffer) + 1);
+        strcpy(moveInfo, buffer);
+        write(1, "\n", 1); 
+
+        int check = checkIfFull(moveInfo);
+
+        if(check == 2)
+        {
+            write(1, "Your opponent repeated a move. Wait for another move...\n", 56);
+            if(send(fd, REPEATED_MOVE, strlen(REPEATED_MOVE), 0) < 0)
+            {
+                write(2, "Sending info to other player failed.\n", 37);
+                exit(1);
+            }
+            playGame(0, playerNum);  
+        }
+        else if(check == 1)
+        {
+            write(1, "Your opponent chose a block with ship!\n", 39);
+            if(send(fd, FULL_BLOCK, strlen(FULL_BLOCK), 0) < 0)
+            {
+                write(2, "Sending info to other player failed.\n", 37);
+                exit(1);
+            }  
+            playGame(0, playerNum); 
+        }
+        else if(check == 0)
+        {
+            write(1, "Your opponent chose an empty block!\n", 36);
+            if(send(fd, EMPTY_BLOCK, strlen(EMPTY_BLOCK), 0) < 0)
+            {
+                write(2, "Sending info to other player failed.\n", 37);
+                exit(1);
+            }  
+            playGame(1, playerNum); 
+        }        
+    }
+}
+
+void readMap(char* fileName)
+{
+    int bytesRead, fd;
+
+    if( (fd = open(fileName, O_RDONLY) ) < 0)
+    {
+        write(2, "Reading map failed.\n", 20);
+        exit(1);
+    }
+
+    bytesRead = read(fd, map, MAP_ROW * 2 * MAP_COLUMN);
+    write(1, "This is how your map looks like: \n", 34);
+    write(1, map, strlen(map));
+    close(fd);
 }
 
 void acceptOpponent()
@@ -84,12 +223,14 @@ void acceptOpponent()
     int len = sizeof(addr);
     if((newGamefd = accept(clientToClientfd, (struct sockaddr *)&addr, &len)) < 0)   
     {   
-        write(2, "Accepting opponent failed.\n", 27);  
-        perror("ACCEPT");   
+        write(2, "Accepting opponent failed.\n", 27);    
         exit(1);   
     }   
     else
-        write(2, "Now wait for the game to begin.\n", 32); 
+        write(1, "Found an opponent! \n", 20);
+        write(1, "Game began: \n", 13);
+        write(1, "It's your turn: \n", 17);
+        playGame(1, 1); 
 }
 
 void establishGameConnection() 
@@ -99,12 +240,16 @@ void establishGameConnection()
     if((connectStatus = connect(gamefd, (struct sockaddr *)&gameAddress,  sizeof(gameAddress))) < 0) 
     {
         write(2, "Connection to opponent failed.\n", 31);
-        perror("CONNECTION");
         exit(1);       
     }
     else
     {
-        write(1, "Successfully connected to opponent! Now wait for the game to begin.\n", 68);
+        write(1, "Successfully connected to opponent! \n", 37);
+        bzero((char *) &buffer, sizeof(buffer));
+        int bytes = recv(clientToServerfd, buffer, BUFFER_SIZE, 0); 
+        if(bytes == 0)
+            close(clientToServerfd);
+        playGame(0, 2); 
     }    
 }
 
@@ -133,9 +278,7 @@ void createFinalGamePort(char* gameInfo)
     }
 
     gameAddress.sin_family = AF_INET; 
-    puts(gameIP);  
     gameAddress.sin_addr.s_addr = inet_addr(gameIP); 
-    puts(gamePort);
     gameAddress.sin_port = htons(atoi(gamePort)); 
 
     establishGameConnection();    
@@ -228,9 +371,14 @@ void waitForAnswer()
 void establishConnection()
 {
     write(1, "Please enter your username.\n", 28);
-    int bytesCount = read(0, name, NAME_MAX_LEN - 2);
+    int bytesCount = read(0, name, BUFFER_SIZE - 2);
     name[bytesCount - 1] = '\0';
     char* dataToBeSent  = concat(IPandPort, name);
+
+    write(1, "Now please enter your map's file name.\n", 39);
+    bytesCount = read(0, fileName, BUFFER_SIZE);
+    fileName[bytesCount - 1] = '\0';
+    readMap(fileName);
 
     char answer[5];
 
@@ -256,7 +404,7 @@ void establishConnection()
         else if(strcmp(answer, "no") == 0)
         {
             dataToBeSent = concat("normal ", dataToBeSent);
-            puts(dataToBeSent);
+            // puts(dataToBeSent);
             sendDataToServer(dataToBeSent);
             waitForAnswer();
             break;
